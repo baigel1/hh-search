d90c3d74d932943bc37c00bb526a21b153fc1067
Daniel Baigel <dbaigel@yext.com>
2025-03-04 14:36:03 -0500

    added clickable links in result cards

--- src/Components/ResultCard.tsx
+++ src/Components/ResultCard.tsx
@@ -1,6 +1,14 @@
 import "./ResultCard.css";
 const ResultCard = ({ result }: any) => {
   console.log(result);
+  const link =
+    result.link ||
+    result.rawData?.landingPageUrl ||
+    result.rawData?.c_primaryCTA?.link ||
+    result.rawData?.c_secondaryCTA?.link ||
+    result.rawData?.website ||
+    result.rawData?.youtube_videoURL ||
+    "";
   return (
     <div className="result-container" id={result.index}>
       <div className="flex gap-1">
@@ -11,7 +19,9 @@ const ResultCard = ({ result }: any) => {
       </div>
       <div className="flex gap-1">
         <p className="font-semibold">Source:</p>
-        <div>{result.name}</div>
+        <div>
+          <a href={link}>{result.name}</a>
+        </div>
       </div>
       <div className="flex gap-1">
         <p className="font-semibold">Relevance Score:</p>
